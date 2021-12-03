955cd621895ac2bb951d443948d19fa615fa8136
Paul Hankin <paul.hankin@pobox.com>
2021-12-03 09:56:29 +0100

    remove test data

--- day3.go
+++ day3.go
@@ -56,21 +56,6 @@ func filterBits(in []uint32, b bool) uint32 {
 	return 0
 }
 
-var intest = []uint32{
-	0b00100,
-	0b11110,
-	0b10110,
-	0b10111,
-	0b10101,
-	0b01111,
-	0b00111,
-	0b11100,
-	0b10000,
-	0b11001,
-	0b00010,
-	0b01010,
-}
-
 func day3() error {
 	in, err := readDay3()
 	if err != nil {
